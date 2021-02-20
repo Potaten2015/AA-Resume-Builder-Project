dca5abd9ee2e5a3d02b2cf1bdc15e7ad50fd18b6
matthewhasan <matthewhasan1218@gmail.com>
2021-02-20 16:46:08 -0600

    got routes and components set up

---
+++ react-app/src/components/MainPage/MainPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./MainPage.css"
+
+const MainPage = () => {
+    return (
+        <h1>Main Page</h1>
+    )
+}
+
+export default MainPage
---
+++ react-app/src/components/Templates/Templates.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Templates.css"
+
+const Templates = () => {
+    return (
+        <h1>Templates Page</h1>
+    )
+}
+
+export default Templates
