5a7e97309d54ccff1a1fcf71b27fe6dd225b681b
Johnny <thejohnnytran27@gmail.com>
2021-02-24 12:00:14 -0600

    clear resumes

--- react-app/src/components/Management/Management.js
+++ react-app/src/components/Management/Management.js
@@ -4,47 +4,31 @@ import * as resumeActions from '../../store/resume';
 import { useDispatch, useSelector } from 'react-redux';
 
 const Management = () => {
-    // 
-    // const [manageResumes, setManageResumes] = useState([])
-    // const [user, setUser] = useState('')
-    const { getResumes } = resumeActions;
+  const { getResumes } = resumeActions;
+  const [loaded, setLoaded] = useState(false);
+  const dispatch = useDispatch();
+  const resumes = useSelector((state) => state.resume.resume);
+  const currentUser = useSelector((state) => state.user);
 
-    const dispatch = useDispatch()
-    const resumes = useSelector((state) => state.resume.resume)
-    const currentUser = useSelector((state) => state.user)
-    // const currentUserId = useSelector((state) => state.user.id)
-    console.log("CURRENT USER", currentUser)
+  useEffect(() => {
+    dispatch(getResumes()).then(() => setLoaded(true));
+    console.log('RESUMES', resumes);
+  }, [dispatch]);
 
-    // useEffect(() => {
-    //     if (currentUser) {
-    //         setUser(currentUser)
-    //     }
-    // }, [currentUser])
+  return (
+    <>
+      <h1>Management Page for {currentUser.username}</h1>
+      {loaded &&
+        resumes &&
+        Object.values(resumes).map((resume) => {
+          return (
+            <>
+              <div dangerouslySetInnerHTML={{ __html: resume.html }} />
+            </>
+          );
+        })}
+    </>
+  );
+};
 
-    useEffect(() => {
-        dispatch(getResumes())
-        // setManageResumes(Object.values(resumes).filter((resume) => resume.user_id === currentUserId))
-    }, [getResumes, dispatch, currentUser])
-
-    
-    // const resumVals = Object.values(resumes)
-    // const usersResumes = resumVals.filter((resume) => resume.user_id === currentUserId)
-    // setManageResumes(usersResumes)
-    // const resumes = useSelector((state) => state.resume.filter((resume) => resume.user_id === currentUserId));
-
-    
-
-    return (
-        <>
-            <h1>Management Page for {currentUser.username}</h1>
-            { resumes && Object.values(resumes).map((resume) => {
-                return (
-                    <>
-                        <div dangerouslySetInnerHTML={{ __html: resume.html }}/>
-                    </>
-                )})}
-        </>
-    )
-}
-
-export default Management
+export default Management;
