d3680ce7640077cfcf1ef55ec1379400dbd592c3
matthewhasan <matthewhasan1218@gmail.com>
2021-02-24 11:49:56 -0600

    deleted commented out code

--- react-app/src/components/Management/Management.js
+++ react-app/src/components/Management/Management.js
@@ -1,38 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import './Management.css';
 import * as resumeActions from '../../store/resume';
 import { useDispatch, useSelector } from 'react-redux';
 
 const Management = () => {
-    // 
-    // const [manageResumes, setManageResumes] = useState([])
-    // const [user, setUser] = useState('')
     const { getResumes } = resumeActions;
 
     const dispatch = useDispatch()
     const resumes = useSelector((state) => state.resume.resume)
     const currentUser = useSelector((state) => state.user)
-    // const currentUserId = useSelector((state) => state.user.id)
-    console.log("CURRENT USER", currentUser)
-
-    // useEffect(() => {
-    //     if (currentUser) {
-    //         setUser(currentUser)
-    //     }
-    // }, [currentUser])
-
+    
     useEffect(() => {
         dispatch(getResumes())
-        // setManageResumes(Object.values(resumes).filter((resume) => resume.user_id === currentUserId))
     }, [getResumes, dispatch, currentUser])
-
-    
-    // const resumVals = Object.values(resumes)
-    // const usersResumes = resumVals.filter((resume) => resume.user_id === currentUserId)
-    // setManageResumes(usersResumes)
-    // const resumes = useSelector((state) => state.resume.filter((resume) => resume.user_id === currentUserId));
-
-    
 
     return (
         <>
