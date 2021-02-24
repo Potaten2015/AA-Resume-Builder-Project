880ab7c3198d20213f3a65a35440453b5dcb0974
Johnny <thejohnnytran27@gmail.com>
2021-02-24 13:28:43 -0600

    e.persist() to setValues of our valueholder object

--- react-app/src/components/EditingPage/EditingPage.js
+++ react-app/src/components/EditingPage/EditingPage.js
@@ -1,28 +1,50 @@
-import React from "react"
-import { useSelector } from "react-redux"
-import "./EditingPage.css"
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import EditHelper from '../EditHelper';
+import './EditingPage.css';
 
 const EditingPage = () => {
+  const current_template = useSelector((state) => state.template.current);
+  const valueHolder = {};
+  for (let i = 0; i < current_template.length; i++) {
+    valueHolder[i] = '';
+  }
+  console.log('VALUE HOLDER', valueHolder);
+  const [values, setValues] = useState(valueHolder);
 
-    const current_template = useSelector(state => state.template.current)
-
-    return (
-        <div className="editing-page">
-            <div className="editing-page-outer">
-                <div className="editing-page-form-container">
-                    <h1>Editing Resume</h1>
-                    <form>
-                        <p>This is the resume form</p>
-                    </form>
+  // useEffect(() => {
+  //   console.log('values', values[0]);
+  // }, []);
+  return (
+    <div className="editing-page">
+      <div className="editing-page-outer">
+        <div className="editing-page-form-container">
+          <h1>Editing Resume</h1>
+          <form>
+            {current_template.map((field, index) => {
+              console.log('VALUES', values);
+              return (
+                <div>
+                  <EditHelper
+                    field={field}
+                    form={true}
+                    value={values[index]}
+                    setValues={setValues}
+                    number={index}
+                    valueHolder={valueHolder}
+                  />
                 </div>
-                <div className="editing-page-preview-container">
-                    <h1>Resume Preview</h1>
-                    <p>This is a preview</p>
-                </div>
-            </div>
+              );
+            })}
+          </form>
         </div>
-    )
-
-}
+        <div className="editing-page-preview-container">
+          <h1>Resume Preview</h1>
+          <p>This is a preview</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default EditingPage;
