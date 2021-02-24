48f5d5f413286fa1041cb4293e9b36c3ea2f0fe3
matthewhasan <matthewhasan1218@gmail.com>
2021-02-24 12:36:52 -0600

    finalized

--- app/api/resume_routes.py
+++ app/api/resume_routes.py
@@ -7,8 +7,16 @@ resume_routes = Blueprint('resumes', __name__)
 
 @resume_routes.route("/", methods=["GET"])
 def get_resumes():
-    resumes = Resume.query.all()
-    resume_html_dict = {}
-    for resume in resumes:
-        resume_html_dict[resume.id] = resume.html
-    return resume_html_dict
+    current_user = int(session['_user_id'])
+    resumes = Resume.query.filter(Resume.user_id == current_user).all()
+    each_resume = {}
+    count = 0
+    for resume in range(0, len(resumes)):
+        each_resume[count] = {
+            "id": resumes[count].id,
+            "html": resumes[count].html,
+            "user_id": resumes[count].user_id,
+            "style_id": resumes[count].style_id,
+        }
+        count += 1
+    return each_resume
