cd5f2163fe8dd276bb95afe1b6daad249d75bb6f
Johnny <thejohnnytran27@gmail.com>
2021-02-25 19:53:21 -0600

    get html

--- react-app/src/components/EditingPage/EditingPage.js
+++ react-app/src/components/EditingPage/EditingPage.js
@@ -3,6 +3,11 @@ import { useSelector } from 'react-redux';
 import EditHelper from '../EditHelper';
 import Preview from '../Templates/Preview';
 import './EditingPage.css';
+import ReactDOM from 'react-dom';
+import ReactDOMServer from 'react-dom/server';
+import { Redirect } from 'react-router-dom';
+// import { Provider } from 'react-redux';
+// import store from '../../store';
 
 const EditingPage = () => {
   const current_template_object = useSelector((state) => state.template.current);
@@ -14,6 +19,29 @@ const EditingPage = () => {
   }
   console.log('VALUE HOLDER', valueHolder);
   const [values, setValues] = useState(valueHolder);
+
+  const printHTML = () => {
+    // const livePreview = ReactDOM.findDOMNode(
+    //   <Preview
+    //     template_name={current_template_name}
+    //     template={current_template}
+    //     values={values}
+    //     preview={true}
+    //   />
+    // );
+    // <Provider store={store}>
+    //   <Preview
+    //     template_name={current_template_name}
+    //     template={current_template}
+    //     values={values}
+    //     preview={false}
+    //   />
+    // </Provider>
+    const livePreview = document.getElementById('resume-to-save');
+    const rawHTML = livePreview.innerHTML;
+    console.log(rawHTML);
+    return <Redirect to="/resumes" />;
+  };
 
   return (
     <div className="editing-page">
@@ -37,10 +65,16 @@ const EditingPage = () => {
               );
             })}
           </form>
+          <button onClick={printHTML}>Print HTML</button>
         </div>
         <div className="editing-page-preview-container">
           <h1>Resume Preview</h1>
-          <Preview template_name={current_template_name} template={current_template} values={values} preview={true} />
+          <Preview
+            template_name={current_template_name}
+            template={current_template}
+            values={values}
+            preview={true}
+          />
         </div>
       </div>
     </div>
